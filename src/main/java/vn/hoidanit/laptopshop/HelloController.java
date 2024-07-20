771f3c37574ffbcda27d45be96fb9400f8cbb38d
tuanphampp9 <tuanphampp9@gmail.com>
2024-07-20 15:50:19 +0700

    hello world

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -0,0 +1,12 @@
+package vn.hoidanit.laptopshop;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World with tuan pham!";
+    }
+}
